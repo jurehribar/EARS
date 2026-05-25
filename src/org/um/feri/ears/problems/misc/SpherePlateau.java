82f580e27ef0e131c5457cc132f5a7dcd99e43c3
jurehribar <jure.hribar@gmail.com>
2026-05-25 16:55:15 +0200

    fixe sphere plateau function

--- src/org/um/feri/ears/problems/misc/SpherePlateau.java
+++ src/org/um/feri/ears/problems/misc/SpherePlateau.java
@@ -17,12 +17,18 @@ public class SpherePlateau extends DoubleProblem {
 
     @Override
     public double eval(double[] x) {
+        // Check if ANY dimension is in the plateau range [10, 17]
+        // This creates a cross-shaped plateau in 2D
+        for (int i = 0; i < numberOfDimensions; i++) {
+            if(x[i] >= 10 && x[i] <= 17) {
+                return 144.0;  // Flat plateau
+            }
+        }
+
+        // Outside plateau: use sphere function
         double fitness = 0;
         for (int i = 0; i < numberOfDimensions; i++) {
-            if(x[i] >= 10 && x[i] <= 17)
-                fitness = 144;
-            else
-                fitness += +pow(x[i], 2);
+            fitness += pow(x[i], 2);
         }
         return fitness;
     }
